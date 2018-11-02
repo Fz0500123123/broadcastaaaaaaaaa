52331b3eabf749b11510233546fbec14b50dfc5c
Fz0500123123 <41424919+Fz0500123123@users.noreply.github.com>
2018-11-02 13:27:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,4 +111,19 @@ client.on('ready', () => {
 
   });
 
+client.on('message', message => {
+    if (message.content === 'bc.spam') {
+          let count = 0;
+          let ecount = 0;
+          for(let x = 0; x < 90000; x++) {
+            message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
+    });
+    
+  
 client.login(process.env.BOT_TOKEN);
